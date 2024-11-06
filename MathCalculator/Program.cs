cdeba1600df0abd2a9569cd4fce5b962bfb0f75a
D3v_Mari0:) <142172035+Maric0sql@users.noreply.github.com>
2024-11-06 12:21:57 -0300

    Add files via upload

---
+++ MathCalculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math Calculator");
+
+            Console.Write("Write the fist number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Choose the operator (+, -, *, /): ");
+            char operators = Console.ReadLine()[0];
+
+            Console.Write("Write the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operators)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        Console.WriteLine("Error, cannot to divide with 0.");
+                    break;
+                default:
+                    Console.WriteLine("Not valid.");
+                    break;
+            }
+
+            Console.WriteLine("Result is: " + result);
+        }
+    }
+}
